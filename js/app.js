81920fba99f798f669ac59de7343fa9bf07f7c02
MinaNabil <minanabilsalib@gmail.com>
2020-09-14 00:12:06 +0200

    first try

--- js/app.js
+++ js/app.js
@@ -17,6 +17,10 @@
  * Define Global Variables
  * 
 */
+const listOfSections = document.querySelectorAll('section');
+const navigationMenu = document.querySelector('#navbar__list');
+let last_known_scroll_position = 0;
+
 
 
 /**
@@ -37,9 +41,26 @@
 
 
 // Add class 'active' to section when near top of viewport
-
+function activateClass()
+{
+    
+}
 
 // Scroll to anchor ID using scrollTO event
+function respondToTheClick(evt)
+ {
+    console.log('An item was clicked: ' + evt.target.textContent);
+    for(let i = 0; i < listOfSections.length; i++)
+    {
+        if (evt.target.textContent===listOfSections[i].dataset.nav) 
+        {
+            window.scrollTo(0,listOfSections[i].offsetTop-140);
+            //activateClass(listOfSections[i]);
+        }
+
+    }
+}
+
 
 
 /**
@@ -49,9 +70,44 @@
 */
 
 // Build menu 
-
+for(let i = 0; i < listOfSections.length; i++){
+    const newLi = document.createElement('li');
+    newLi.className="menu__link";
+    newLi.innerText=listOfSections[i].dataset.nav;
+    navigationMenu.appendChild(newLi);
+}
 // Scroll to section on link click
+navigationMenu.addEventListener('click', respondToTheClick);
 
 // Set sections as active
+document.addEventListener('scroll',function(){
+    last_known_scroll_position=window.scrollY;
+    for(let i = 0; i < listOfSections.length; i++)
+    {
+        if (i==listOfSections.length-1)
+        {
+            if (last_known_scroll_position>=(listOfSections[i].offsetTop-150))
+            {
+                listOfSections[i].className='your-active-class';
 
+            }
+            else
+            {
+                listOfSections[i].className='';
+            }
+        } 
+        else
+        {   
+        if (last_known_scroll_position>=(listOfSections[i].offsetTop-150) && last_known_scroll_position < (listOfSections[i+1].offsetTop-150) ) 
+            {
+                listOfSections[i].className='your-active-class';
+            }
+            else
+            {
+                listOfSections[i].className='';
+            }
+        }
+    }
+
+});
 
